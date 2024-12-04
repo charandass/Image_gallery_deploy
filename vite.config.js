2a844ced1ebf1d945d9fd3493783d2e433672fe6
charandass <charan1631@gmail.com>
2024-12-04 11:15:45 +0530

    updating package.json

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Image_gallery_deploy/"
+  base: "/image_gallery_deploy/"
 })
